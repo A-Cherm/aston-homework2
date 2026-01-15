7391b6158923d02345ed204be6a0f8c5346980b4
AChermnykh <achermnykh@yandex.ru>
2026-01-15 15:42:49 +0300

    Добавить gateway

---
+++ gateway/src/main/java/ru/acherm/astonhw2/Gateway.java
@@ -0,0 +1,11 @@
+package ru.acherm.astonhw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gateway {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class, args);
+    }
+}
